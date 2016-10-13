f1ed5f30c66392679868f5bc7926e823379adab2
stefano <stefano@debian>
2016-10-13 21:15:29 +0100

    some more code

--- ebayFricApp.py
+++ ebayFricApp.py
@@ -2,12 +2,6 @@ import requests
 import json
 from urllib import urlencode
 
-# active listing search payload
-ebayFindinghUrl = "http://svcs.ebay.co.uk/services/search/FindingService/v1?OPERATION-NAME=findItemsAdvanced&SERVICE-VERSION=1.13.0&SECURITY-APPNAME=StefanoR-ebayFric-PRD-19f17700d-ff298548&RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD&"
-efPayload = { 'itemFilter(0).name' : 'Seller',
-             'itemFilter(0).value' : 'al-rawda',
-             'paginationInput.entriesPerPage' : '2' }
-
 
 # items details retrieval
 ebayShoppingUrl = "http://open.api.ebay.com/shopping?"
@@ -20,22 +14,12 @@ esPayload = { 'appid' : 'StefanoR-ebayFric-PRD-19f17700d-ff298548',
 
 
 
-url = ebayFindinghUrl + urlencode(efPayload)
-
-r = requests.get(url)
-j = json.loads(r.text)
-totResults = int(j['findItemsAdvancedResponse'][0]['searchResult'][0]['@count'])
-totPages = int(j['findItemsAdvancedResponse'][0]['paginationOutput'][0]['totalPages'][0])
-pageNr = int(j['findItemsAdvancedResponse'][0]['paginationOutput'][0]['pageNumber'][0])
-resultPerPage = int(j['findItemsAdvancedResponse'][0]['paginationOutput'][0]['entriesPerPage'][0])
-
-
-def getItemsFromSeller(sellerId, resultsPerPage=2):
+def getItemsFromSeller(sellerId, resultsPerPage=100):
     ebayFindinghUrl = "http://svcs.ebay.co.uk/services/search/FindingService/v1?OPERATION-NAME=findItemsAdvanced&SERVICE-VERSION=1.13.0&SECURITY-APPNAME=StefanoR-ebayFric-PRD-19f17700d-ff298548&RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD&"
     efPayload = { 'itemFilter(0).name' : 'Seller',
                  'itemFilter(0).value' : sellerId,
                  'paginationInput.entriesPerPage' : resultsPerPage,
-                 'paginationInput.pageNumber' : 2 }
+                 'paginationInput.pageNumber' : 1 }
     
     url = ebayFindinghUrl + urlencode(efPayload)
     r = requests.get(url)
@@ -43,14 +27,17 @@ def getItemsFromSeller(sellerId, resultsPerPage=2):
     totResults = int(j['findItemsAdvancedResponse'][0]['paginationOutput'][0]['totalEntries'][0])
     totPages = int(j['findItemsAdvancedResponse'][0]['paginationOutput'][0]['totalPages'][0])
     pageNr = int(j['findItemsAdvancedResponse'][0]['paginationOutput'][0]['pageNumber'][0])
-    resultPerPage = int(j['findItemsAdvancedResponse'][0]['paginationOutput'][0]['entriesPerPage'][0])
-    itemsList = []
-    for item in j['findItemsAdvancedResponse'][0]['searchResult'][0]['item']:
-        itemsList.append(item['itemId'][0])
-        
-    while len(itemsList) < totResults:
-        pass
-        
-        
     
+    if 'item' in j['findItemsAdvancedResponse'][0]['searchResult'][0].keys():
+        itemsList = []
+        for item in j['findItemsAdvancedResponse'][0]['searchResult'][0]['item']:
+            itemsList.append(item['itemId'][0])
+            
+        while len(itemsList) < totResults:
+            efPayload['paginationInput.pageNumber'] += 1
+            url = ebayFindinghUrl + urlencode(efPayload)
+            r = requests.get(url)
+            j = json.loads(r.text)
+            for item in j['findItemsAdvancedResponse'][0]['searchResult'][0]['item']:
+                itemsList.append(item['itemId'][0])
     
