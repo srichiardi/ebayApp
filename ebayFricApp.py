0bb83be5a07736073564da24323eabd22f3b300d
srichiardi <srichiardi@L-DUB-13002487.corp.ebay.com>
2016-11-10 10:39:19 +0000

    small correction to gerNrOfSold

--- ebayFricApp.py
+++ ebayFricApp.py
@@ -2,18 +2,18 @@ import requests
 import json
 from urllib import urlencode
 
-def getItemsFromSeller(sellerId, resultsPerPage=100):
+def getItemsFromSeller(sellerId, totResults=10):
     ebayFindinghUrl = "http://svcs.ebay.co.uk/services/search/FindingService/v1?OPERATION-NAME=findItemsAdvanced&SERVICE-VERSION=1.13.0&SECURITY-APPNAME=StefanoR-ebayFric-PRD-19f17700d-ff298548&RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD&"
     efPayload = { 'itemFilter(0).name' : 'Seller',
                  'itemFilter(0).value' : sellerId,
-                 'paginationInput.entriesPerPage' : resultsPerPage,
+                 'paginationInput.entriesPerPage' : 100,
                  'paginationInput.pageNumber' : 1 }
     
     
     url = ebayFindinghUrl + urlencode(efPayload)
     r = requests.get(url)
     j = json.loads(r.text)
-    totResults = int(j['findItemsAdvancedResponse'][0]['paginationOutput'][0]['totalEntries'][0])
+    #totResults = int(j['findItemsAdvancedResponse'][0]['paginationOutput'][0]['totalEntries'][0])
     totPages = int(j['findItemsAdvancedResponse'][0]['paginationOutput'][0]['totalPages'][0])
     pageNr = int(j['findItemsAdvancedResponse'][0]['paginationOutput'][0]['pageNumber'][0])
     itemsList = []
@@ -37,16 +37,23 @@ def getItemsFromSeller(sellerId, resultsPerPage=100):
 
 
 def getNrOfSold(listOfItems):
+    # dedup listOfItems
+    setOfItems = list(set(listOfItems))
+    asciiItemsStr = setOfItems[0]
+    for item in setOfItems[1:]:
+        asciiItemsStr += ',' + item
+        
     ebayShoppingUrl = "http://open.api.ebay.com/shopping?"
     esPayload = { 'appid' : 'StefanoR-ebayFric-PRD-19f17700d-ff298548',
             'callname' : 'GetMultipleItems',
             'version' : '975',
             'responseencoding' : 'JSON',
-            'ItemID' : listOfItems,
+            'ItemID' : asciiItemsStr,
             'IncludeSelector' : 'Details'}
     url = ebayShoppingUrl + urlencode(esPayload)
     r = requests.get(url)
     j = json.loads(r.text)
+        
     itemsList = []
     for item in j['Item']:
         itemDict = {}
