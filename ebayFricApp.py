d3d89853a41e1d63b122e81c74f26aeb1faed1c6
srichiardi <srichiardi@L-DUB-13002487.corp.ebay.com>
2016-12-13 16:19:38 +0000

    tested improved version

--- ebayFricApp.py
+++ ebayFricApp.py
@@ -1,27 +1,43 @@
 import os
+import sys
 import csv
 import requests
 import json
+import time
 from urllib import urlencode
+from ebayAppWidgets import appDlg
 
-def getItemsFromSeller(sellerId, totResults=10):
-    ebayFindinghUrl = "http://svcs.ebay.co.uk/services/search/FindingService/v1?OPERATION-NAME=findItemsAdvanced&SERVICE-VERSION=1.13.0&SECURITY-APPNAME=StefanoR-ebayFric-PRD-19f17700d-ff298548&RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD&"
+def getItemsFromSeller(searchOptions):
+
     efPayload = { 'itemFilter(0).name' : 'Seller',
-                  'itemFilter(0).value' : sellerId,
-                  'SoldItemsOnly' : 'true',
-                  'paginationInput.entriesPerPage' : 100,
-                  'paginationInput.pageNumber' : 1 }
+                  'itemFilter(0).value' : searchOptions['sellerId'] }
     
+    if 'totResults' in searchOptions.keys():
+        efPayload['paginationInput.entriesPerPage'] = searchOptions['totResults']
+    else:
+        efPayload['paginationInput.entriesPerPage'] = 100
+
+    if 'startFromPage' in searchOptions.keys():
+        efPayload['paginationInput.pageNumber'] = int(searchOptions['startFromPage'])
+    else:
+        efPayload['paginationInput.pageNumber'] = 1
+    
+    if 'soldOnly' in searchOptions.keys():
+        efPayload['SoldItemsOnly'] = searchOptions['soldOnly']
+
+    if 'keywords' in searchOptions.keys():
+        efPayload['keywords'] = searchOptions['keywords']
+        
+    ebayFindinghUrl = "http://svcs.ebay.co.uk/services/search/FindingService/v1?OPERATION-NAME=findItemsAdvanced&SERVICE-VERSION=1.13.0&SECURITY-APPNAME=StefanoR-ebayFric-PRD-19f17700d-ff298548&RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD&"
     
     url = ebayFindinghUrl + urlencode(efPayload)
     r = requests.get(url)
     j = json.loads(r.text)
-    #totResults = int(j['findItemsAdvancedResponse'][0]['paginationOutput'][0]['totalEntries'][0])
+    totResults = int(j['findItemsAdvancedResponse'][0]['paginationOutput'][0]['totalEntries'][0])
     totPages = int(j['findItemsAdvancedResponse'][0]['paginationOutput'][0]['totalPages'][0])
     pageNr = int(j['findItemsAdvancedResponse'][0]['paginationOutput'][0]['pageNumber'][0])
     itemsList = []
 
-    print "found %d items" % totResults
     
     if 'item' in j['findItemsAdvancedResponse'][0]['searchResult'][0].keys():
         for item in j['findItemsAdvancedResponse'][0]['searchResult'][0]['item']:
@@ -41,40 +57,40 @@ def getItemsFromSeller(sellerId, totResults=10):
 
 def getNrOfSold(listOfItems):
     # dedup listOfItems
-    setOfItems = list(set(listOfItems))
-    asciiItemsStr = setOfItems[0]
-    for item in setOfItems[1:]:
-        asciiItemsStr += ',' + item
-        
-    ebayShoppingUrl = "http://open.api.ebay.com/shopping?"
-    esPayload = { 'appid' : 'StefanoR-ebayFric-PRD-19f17700d-ff298548',
-            'callname' : 'GetMultipleItems',
-            'version' : '975',
-            'responseencoding' : 'JSON',
-            'ItemID' : asciiItemsStr,
-            'IncludeSelector' : 'Details'}
-    url = ebayShoppingUrl + urlencode(esPayload)
-    r = requests.get(url)
-    j = json.loads(r.text)
-        
     itemsList = []
-    for item in j['Item']:
-        itemDict = {}
-        itemDict["Item_id"] = item["ItemID"]
-        itemDict["ListingStatus"] = item["ListingStatus"]
-        itemDict["Location"] = item["Location"]
-        itemDict["Price"] = item["CurrentPrice"]["Value"]
-        itemDict["Currency"] = item["CurrentPrice"]["CurrencyID"]
-        itemDict["Quantity_sold"] = item["QuantitySold"]
-        itemDict["Title"] = item["Title"]
-        itemDict["Quantity"] = item["Quantity"]
-        itemDict["Seller_id"] = item["Seller"]["UserID"]
-        itemsList.append(itemDict)
+    setOfItems = list(set(listOfItems))
+    if len(setOfItems) > 0:
+        asciiItemsStr = ','.join(setOfItems)
+            
+        ebayShoppingUrl = "http://open.api.ebay.com/shopping?"
+        esPayload = { 'appid' : 'StefanoR-ebayFric-PRD-19f17700d-ff298548',
+                'callname' : 'GetMultipleItems',
+                'version' : '975',
+                'responseencoding' : 'JSON',
+                'ItemID' : asciiItemsStr,
+                'IncludeSelector' : 'Details'}
+        url = ebayShoppingUrl + urlencode(esPayload)
+        r = requests.get(url)
+        j = json.loads(r.text)
+        
+        for item in j['Item']:
+            itemDict = {}
+            itemDict["Item_id"] = item["ItemID"]
+            itemDict["ListingStatus"] = item["ListingStatus"]
+            itemDict["Location"] = item["Location"]
+            itemDict["Price"] = item["CurrentPrice"]["Value"]
+            itemDict["Currency"] = item["CurrentPrice"]["CurrencyID"]
+            itemDict["Quantity_sold"] = item["QuantitySold"]
+            itemDict["Title"] = item["Title"]
+            itemDict["Quantity"] = item["Quantity"]
+            itemDict["Seller_id"] = item["Seller"]["UserID"]
+            itemDict["PictureURL"] = item["PictureURL"]
+            itemsList.append(itemDict)
     return itemsList
 
 
-def writeItemsToCsv(itemsList):
-    filePath = os.getcwd() + "\\AppResults.csv"
+def writeItemsToCsv(outputPath, itemsList):
+    filePath = outputPath + "/eBayItemDetails_%s.csv" % time.strftime("%Y%m%d-%H%M%S")
     fileToWrite = open(filePath, 'ab')
     fieldnames = itemsList[0].keys()
     csvWriter = csv.DictWriter(fileToWrite, fieldnames, restval='',
@@ -85,4 +101,16 @@ def writeItemsToCsv(itemsList):
     fileToWrite.close()
 
 
-# aggiungere SoldItemsOnly nella ricerca e status active/inactive in output + parole chiave
+def main():
+    options = appDlg().mainloop()
+    if options['sellerId'] == '':
+        print "missing seller ID!"
+        sys.exit()
+    else:
+        itemsList = getItemsFromSeller(options)
+        itemsDesc = getNrOfSold(itemsList)
+        if len(itemsDesc) > 0:
+            writeItemsToCsv(options['outputFolder'], itemsDesc)
+        else:
+            print "no items found"
+
