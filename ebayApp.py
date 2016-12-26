796b588d7a5d9c902d3f89c2aee75ae34f998fd3
srichiardi <srichiardi@L-DUB-13002487.corp.ebay.com>
2016-12-26 21:15:14 +0000

    implemented last requirements

--- ebayApp.py
+++ ebayApp.py
@@ -35,6 +35,8 @@ def getItemsFromSeller(searchOptions):
     
     for site in searchOptions['sites']:
         ebayFindinghUrl += globalSiteMap[site]['globalID'] + "&"
+        
+        # pulling results from every page
         while True:
             url = ebayFindinghUrl + urlencode(efPayload)
             r = requests.get(url)
@@ -42,50 +44,82 @@ def getItemsFromSeller(searchOptions):
             totPages = int(j['findItemsAdvancedResponse'][0]['paginationOutput'][0]['totalPages'][0])
             pageNr = int(j['findItemsAdvancedResponse'][0]['paginationOutput'][0]['pageNumber'][0])
             totResults = int(j['findItemsAdvancedResponse'][0]['paginationOutput'][0]['totalEntries'][0])
-            if totResults == 0: break
-            for item in j['findItemsAdvancedResponse'][0]['searchResult'][0]['item']:
-                if item['itemId'][0] in itemsDict.keys():
-                    itemsDict[ item['itemId'][0] ]['globalID'].append(item['globalId'][0])
-                else:
-                    itemsDict[ item['itemId'][0] ] = { 'globalID' : item['globalId'][0] }
             
+            # break while loop if there are no results in the current site
+            if totResults == 0: break
+            
+            # save item id in a dictionary key = item, value = list of sites
+            for item in j['findItemsAdvancedResponse'][0]['searchResult'][0]['item']:
+                try:
+                    itemsDict[item].append(site)
+                except KeyError:
+                    itemsDict[item] = [site]
+                    
+            # break while loop if reached last page
+            if totPages == pageNr: break
+            
+            efPayload['paginationInput.pageNumber'] += 1
             
     return itemsDict
 
 
-def getNrOfSold(listOfItems):
-    # dedup listOfItems
+def getNrOfSold(dictOfItems):
+    
+    itemsBySite = {}
+    sitesByitem = {}
     itemsList = []
-    setOfItems = list(set(listOfItems))
-    if len(setOfItems) > 0:
-        asciiItemsStr = ','.join(setOfItems)
+    
+    for site in globalSiteMap.keys():
+        for item in dictOfItems.keys():
+            if site in dictOfItems[item]:
+                try:
+                    itemsBySite[site].append(item)
+                except KeyError:
+                    itemsBySite[site] = [item]
+                finally:
+                    sitesByitem[item] = dictOfItems.pop(item)
+    
+    # requests are made in batches based on site id
+    for site in itemsBySite.keys():
+        siteID = globalSiteMap[site]['siteID']
+        
+        # split nr of item in single request if > 20 per request
+        maxItems = 20
+        ibs = itemsBySite[site]
+        itemsMatrix = [ibs[i:i+maxItems] for i in range(0,len(ibs),maxItems)]
+        
+        for ilist in itemsMatrix:
+            asciiItemsStr = ','.join(ilist)
+                
+            ebayShoppingUrl = "http://open.api.ebay.com/shopping?"
+            esPayload = { 'appid' : 'StefanoR-ebayFric-PRD-19f17700d-ff298548',
+                    'callname' : 'GetMultipleItems',
+                    'version' : '975',
+                    'responseencoding' : 'JSON',
+                    'ItemID' : asciiItemsStr,
+                    'IncludeSelector' : 'Details',
+                    'siteid' : siteID }
             
-        ebayShoppingUrl = "http://open.api.ebay.com/shopping?"
-        esPayload = { 'appid' : 'StefanoR-ebayFric-PRD-19f17700d-ff298548',
-                'callname' : 'GetMultipleItems',
-                'version' : '975',
-                'responseencoding' : 'JSON',
-                'ItemID' : asciiItemsStr,
-                'IncludeSelector' : 'Details',
-                'siteid' : '0' }
-        
-        url = ebayShoppingUrl + urlencode(esPayload)
-        r = requests.get(url)
-        j = json.loads(r.text)
-        
-        for item in j['Item']:
-            itemDict = {}
-            itemDict["Item_id"] = item["ItemID"]
-            itemDict["ListingStatus"] = item["ListingStatus"]
-            itemDict["Location"] = item["Location"]
-            itemDict["Price"] = item["CurrentPrice"]["Value"]
-            itemDict["Currency"] = item["CurrentPrice"]["CurrencyID"]
-            itemDict["Quantity_sold"] = item["QuantitySold"]
-            itemDict["Title"] = item["Title"]
-            itemDict["Quantity"] = item["Quantity"]
-            itemDict["Seller_id"] = item["Seller"]["UserID"]
-            itemDict["PictureURL"] = item["PictureURL"]
-            itemsList.append(itemDict)
+            url = ebayShoppingUrl + urlencode(esPayload)
+            r = requests.get(url)
+            j = json.loads(r.text)
+            
+            for item in j['Item']:
+                itemDict = {}
+                itemDict["Item_id"] = item["ItemID"]
+                itemDict["ListingStatus"] = item["ListingStatus"]
+                itemDict["Location"] = item["Location"]
+                itemDict["Price"] = item["CurrentPrice"]["Value"]
+                itemDict["Currency"] = item["CurrentPrice"]["CurrencyID"]
+                itemDict["Quantity_sold"] = item["QuantitySold"]
+                itemDict["Title"] = item["Title"]
+                itemDict["Quantity"] = item["Quantity"]
+                itemDict["Seller_id"] = item["Seller"]["UserID"]
+                itemDict["PictureURL"] = item["PictureURL"]
+                itemDict["OriginalURL"] = item["ViewItemURLForNaturalSearch"]
+                itemDict["Sites"] = ', '.join(sitesByitem[item])
+                itemsList.append(itemDict)
+            
     return itemsList
 
 
@@ -126,8 +160,8 @@ if __name__ == "__main__":
 Add Items Ended
 Map siteIDs to GlobalIDs in seller search and item search calls
 Add searches in Global eBay sites
-Add link to the listing
-Add picturesURL to output
+Add link to the listing DONE
+Add picturesURL to output DONE
 '''
 
 ##### REFERENCES #####
